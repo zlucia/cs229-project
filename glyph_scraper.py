e38f45f69876878f7928ea30e9b965bf05f34c65
Lucia Zheng <zlucia@stanford.edu>
2019-12-01 23:04:53 -0800

    Revised scraper for compatibility with Google Fonts source url

--- glyph_scraper.py
+++ glyph_scraper.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import requests, io, zipfile
+import time
+import os
+import string
+from PIL import Image, ImageFont
 from collections import OrderedDict
 from bs4 import BeautifulSoup
 from importlib import reload
@@ -8,18 +12,14 @@ import util
 reload(util)
 
 data = util.FontData
-FontDataset = util.FontDataset
 data.load()
 font_names_full = pd.DataFrame(np.sort(data.get_all_name('all'), axis=None).astype(str))
 #print(font_names_full)
-font_names_link = pd.DataFrame(font_names_full.iloc[:, 0].str.split().str[0:-1].str.join('-').str.lower())
+font_names_link = pd.DataFrame(font_names_full.iloc[:, 0].str.split().str[0:-1].str.join('+'))
 font_names_link = font_names_link.drop_duplicates().reset_index(drop=True)
 #print(font_names_link)
 
-ff_url = 'https://www.1001freefonts.com/'
-fs_url = 'https://www.fontsquirrel.com/fonts/download/'
-urls = [ff_url, fs_url]
-outfile_path = './font_files/'
+gwf_url = 'https://fonts.google.com/download?family='
 
 mapping = OrderedDict([
 		('Thin', '100'),
@@ -43,17 +43,17 @@ mapping = OrderedDict([
 		('Oblique', 'italic'),
 	])
 
-def get_download_url(url, font_name):
-	download_url = ''
-	if url == ff_url:
-		search_url = url + font_name + '.font'
-		response = requests.get(search_url)
-		soup = BeautifulSoup(response.text, 'html.parser')
-		url_suffix = soup.select_one('a[href^="/d/"]')['href']
-		download_url = url + url_suffix[1:]
-	elif url == fs_url:
-		download_url = url + font_name
-	return download_url
+# def get_download_url(url, font_name):
+# 	download_url = ''
+# 	if url == ff_url:
+# 		search_url = url + font_name + '.font'
+# 		response = requests.get(search_url)
+# 		soup = BeautifulSoup(response.text, 'html.parser')
+# 		url_suffix = soup.select_one('a[href^="/d/"]')['href']
+# 		download_url = url + url_suffix[1:]
+# 	elif url == fs_url:
+# 		download_url = 
+# 	return download_url
 
 def get_font_name_full(file):
 	name_tokens = file[0:-4].split('-')
@@ -65,25 +65,22 @@ def get_font_name_full(file):
 	name = ' '.join(name_tokens)
 	return name
 
-# Downloads OTF/TTF files
+# Downloads font files (.otf/.ttf)
+fontfiles_path = './font_files/'
+if not os.path.exists(fontfiles_path):
+	os.makedirs(fontfiles_path)
 # for index in font_names_link.index:
 #   font_name = font_names_link.iloc[index, 0]
-font_name = 'abeezee'
-for url in urls:
-	download_url = get_download_url(url, font_name)
-
-	try:
-		r = requests.get(download_url)
-		r.raise_for_status()
-		z = zipfile.ZipFile(io.BytesIO(r.content))
-		for file in z.namelist():
-			name = get_font_name_full(file)
-			# Extract font files for font names in font_names_full
-			if name in font_names_full.values:
-				z.extract(file, outfile_path)
-		break
-	except requests.exceptions.HTTPError as err:
-		if url == urls[-1]:
-			print('Font not found')
-
-# .png files of all glyphs from .otf and .ttf
+font_name = 'ABeeZee'
+download_url = gwf_url + font_name
+try:
+	r = requests.get(download_url)
+	r.raise_for_status()
+	z = zipfile.ZipFile(io.BytesIO(r.content))
+	for file in z.namelist():
+		name = get_font_name_full(file)
+		# Extract font files for font names in dataset
+		if name in font_names_full.values:
+			z.extract(file, fontfiles_path)
+except requests.exceptions.HTTPError as err:
+	print(err)
