5e97f1e0a7d8bea7588977acb6220257b149925e
Lucia Zheng <zlucia@stanford.edu>
2019-12-01 17:30:15 -0800

    Extract font files for font names in dataset

--- glyph_scraper.py
+++ glyph_scraper.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import requests, io, zipfile
+from collections import OrderedDict
 from bs4 import BeautifulSoup
 from importlib import reload
 import util
@@ -18,6 +19,29 @@ font_names_link = font_names_link.drop_duplicates().reset_index(drop=True)
 ff_url = 'https://www.1001freefonts.com/'
 fs_url = 'https://www.fontsquirrel.com/fonts/download/'
 urls = [ff_url, fs_url]
+outfile_path = './font_files/'
+
+mapping = OrderedDict([
+		('Thin', '100'),
+		('ExtraLight', '200'),
+		('Extralight', '200'),
+		('Light', '300'),
+		('Regular', 'regular'),
+		('Medium', '500'),
+		('SemiBold', '600'),
+		('Semibold', '600'),
+		('Bold', '700'),
+		('ExtraBold', '800'),
+		('Extrabold', '800'),
+		('UltraBold', '800'),
+		('Ultrabold', '800'),
+		('Black', '900'),
+		('OSF', ''),
+		('Condensed', ''),
+		('Italic', 'italic'),
+		('It', 'italic'),
+		('Oblique', 'italic'),
+	])
 
 def get_download_url(url, font_name):
 	download_url = ''
@@ -31,23 +55,35 @@ def get_download_url(url, font_name):
 		download_url = url + font_name
 	return download_url
 
+def get_font_name_full(file):
+	name_tokens = file[0:-4].split('-')
+	for w, rw in mapping.items():
+		if w in name_tokens[-1]:
+			name_tokens[-1] = name_tokens[-1].replace(w, rw)
+			if w == 'Condensed':
+				name_tokens[0] += 'Condensed'
+	name = ' '.join(name_tokens)
+	return name
+
 # Downloads OTF/TTF files
 # for index in font_names_link.index:
 #   font_name = font_names_link.iloc[index, 0]
-font_name = 'abhaya-libre'
+font_name = 'abeezee'
 for url in urls:
 	download_url = get_download_url(url, font_name)
 
 	try:
 		r = requests.get(download_url)
 		r.raise_for_status()
-		outfile_path = './font_files/' + font_name
 		z = zipfile.ZipFile(io.BytesIO(r.content))
-		z.extractall(outfile_path)
+		for file in z.namelist():
+			name = get_font_name_full(file)
+			# Extract font files for font names in font_names_full
+			if name in font_names_full.values:
+				z.extract(file, outfile_path)
 		break
 	except requests.exceptions.HTTPError as err:
 		if url == urls[-1]:
 			print('Font not found')
 
-# Search through all downloaded files for fonts in font_names_full
 # .png files of all glyphs from .otf and .ttf
