48ca10fdb1f662534977b7f2fea84b6475830842
smirchan <smirchan@gmail.com>
2019-12-11 16:21:58 -0800

    Updated experiment.py

--- experiment.py
+++ experiment.py
@@ -3,6 +3,19 @@ import torch
 import torch.nn.functional as F
 from sklearn.metrics import r2_score
 
+
+def RMSE(y_pred, y):
+    mse = F.mse_loss(y_pred.float(), y.float(), reduction="mean")
+    return torch.sqrt(mse)
+
+def R2(y_pred, y):
+    return r2_score(y, y_pred)
+
+metrics = {
+    'RMSE': RMSE,
+    'R2': R2
+}
+
 def run_experiment(model, test_x, test_y, torch_model=False):
     # not fully functional with torch models yet
     if torch_model:
@@ -12,13 +25,13 @@ def run_experiment(model, test_x, test_y, torch_model=False):
         predictions = torch.Tensor(predictions)
         test_y = torch.Tensor(test_y)
 
-    error = torch.sqrt(F.mse_loss(predictions, test_y))  # L2 distance between predictions, ground truth
+    error = RMSE(y_pred, y)  # L2 distance between predictions, ground truth
     
     error = error.item()
     if torch_model:
         predictions = np.array(predictions.detach())
 
-    r2 = r2_score(test_y, predictions)
+    r2 = R2(predictions, test_y)
 
     stats = {'RMSE': error,
              'R2': r2,
